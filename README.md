5480b607a09e26aee1bd89ac5c0ff9077b079be4
Shajal Ahamed <shajalahamedcse@gmail.com>
2019-06-11 13:32:36 +0600

    Added simple server in golang

--- README.md
+++ README.md
@@ -4,5 +4,14 @@
 
 Images are binary snapshots. Any kind of container is created from an image. This image contains everything needed to run the application. So when we run any instance of an image that is called container.
 
-In this project , First I will try to build a docker image for a Golang application. Then I will try to attach a `docker volume`  with the container to store application logs. And in the final stage ,will do some optimization of this docker images using multistage docker build concept.
+In this project , First we will try to build a docker image for a Golang application. Then we will try to attach a `docker volume`  with the container to store application logs. And in the final stage ,will do some optimization of this docker images using multistage docker build concept.
 
+### Creating a Simple Golang App
+
+Let's create a simple golang application that we'll containerize.
+
+    $ mkdir -p $GOPATH/src/github.com/shajalahamedcse/godocker
+    $ cd $GOPATH/src/github.com/callicoder/go-docker
+    $ touch server.go
+
+This will be a simple server that will just tell the health of the server. Let's write it now.
---
+++ server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	// Route handlers
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", healthCheckHandler)
+	// Create Server
+	srv := &http.Server{
+		Handler:      route,
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start Server
+	go func() {
+		log.Println("Starting Server")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("It is working")))
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
+}
